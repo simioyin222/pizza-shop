78bcd2a2f2b4ba4f905af43aa479ea049ddf6954
Simi O <coolkid1984@ymail.com>
2023-11-03 15:52:44 -0500

    all test working correctly now, 3rd test pass, fix syntax issue

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 function Pizza(toppings, size) {
-  this.toppings = toppings;
-  this.size = size;
+  this.toppings = toppings
+  this.size = size
 }
 
-Pizza.prototype.calculateCost = function() {
-  const basePrice = 10;
-  const toppingPrice = 2;
-  let sizeMultiplier = 1;
+Pizza.prototype.calculateCost = function () {
+  const basePrice = 10
+  const toppingPrice = 2
+  let sizeMultiplier = 1
 
-  default:
-      console.error('Invalid size');
-      return 0;
+  switch (this.size) {
+    case "small":
+      sizeMultiplier = 0.8
+      break
+
+    default:
+      console.error("Invalid size")
+      return 0
+  }
 }
