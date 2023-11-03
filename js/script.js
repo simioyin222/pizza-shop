022b8195b472614200c21b5777dc924b5ced5edf
Simi O <coolkid1984@ymail.com>
2023-11-03 14:58:42 -0500

    second test pass

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
 }
+
+Pizza.prototype.calculateCost = function() {
+  const basePrice = 10;
+  const toppingPrice = 2;
+  let sizeMultiplier = 1;
+
+  default:
+      console.error('Invalid size');
+      return 0;
+}
